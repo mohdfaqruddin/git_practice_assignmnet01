e26cfd43c621c52014de6aaa9f689a6fffb6b5d2
mohd faqruddin <babakhanre@gmail.com>
2022-09-29 10:57:19 +0530

    added prime and palindrome problems

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str="aman";
+function reverse(string){
+let bag="";
+for(let i=str.length-1;i>=0;i--){
+bag=bag+string[i];
+}
+returnbag;
+}
+let rev_str=reverse(str);
+}
+else
---
+++ prime.js
@@ -0,0 +1,14 @@
+
+let flag=false;
+function prime(num){
+
+for(i=0;i<=num.length-1;i++){
+if(N%i==0){
+flag=true;
+}
+else{
+flase;
+}
+}
+}
+prime(13);
